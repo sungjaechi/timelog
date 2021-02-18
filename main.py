2a4289246886c1c5f781cfbf1caa42eb8097dea5
Sungjae Chi <sungjaec@princeton.edu>
2021-02-18 15:13:06 +0900

    Init Log daily usage seconds per user

---
+++ main.py
@@ -0,0 +1,81 @@
+import discord
+import pandas as pd
+import datetime
+import os
+import pickle
+
+
+class MyClient(discord.Client):
+	def __init__(self):
+		super().__init__()
+		self.path = './data.csv'
+		self.temp_path = './temp.pkl'
+		self.members = []
+		self.temp = {}
+		self.data = pd.DataFrame()
+		self.start_hour = 0
+
+	async def on_ready(self):
+		print('Logged on as {0}!'.format(self.user))
+		if os.path.exists(self.path):
+			self.data = pd.read_csv(self.path, index_col=0)
+			self.members = self.data.columns.to_list()
+		if os.path.exists('./start.csv'):
+			self.start_hour = int(pd.read_csv('./start.csv', index_col=0).loc[0, '0'])
+		if os.path.exists(self.temp_path):
+			a_file = open(self.temp_path, "rb")
+			self.temp = pickle.load(a_file)
+
+	async def on_message(self, message):
+		print('Message from {0.author}: {0.content}'.format(message))
+
+		if message.content.startswith('log'):
+			_, cmd, *argv = message.content.strip().split()
+			if cmd == 'help':
+				await message.channel.send(
+					"log start n: set a day's start n:00\\ log today : show user's spent time in voice channel today")
+
+			if cmd == 'start':
+				self.start_hour = int(argv[0])
+				df = pd.DataFrame([self.start_hour])
+				print(df)
+				pd.DataFrame([self.start_hour]).to_csv('./start.csv')
+
+			if cmd == 'today':
+				date = str((datetime.datetime.now() - datetime.timedelta(hours=self.start_hour)).date())
+				await message.channel.send(str(self.data.loc[date, message.author.name] / 60) + '분')
+
+			if cmd == 'user':
+				for member in message.mentions:
+					await message.channel.send(self.data.sort_index()[member.name])
+
+	async def on_voice_state_update(self, member, before, after):
+		print(self.temp)
+		print(member.name in self.temp.keys())
+		print(self.members)
+		print(self.data)
+		if member.name not in self.members:
+			self.members.append(member.name)
+			self.data[member.name] = [0 for _ in self.data.index]
+
+		if before is not None:
+			if member.name in self.temp.keys():
+				time_in = self.temp[member.name]
+				time_out = datetime.datetime.now()
+				date = str((time_out - datetime.timedelta(hours=self.start_hour)).date())
+				if date not in self.data.index:
+					self.data.loc[date] = [0 for _ in self.members]
+					self.data.loc[date] = [0 for _ in self.members]
+				self.data.loc[date, member.name] += (time_out - time_in).total_seconds()
+
+		if after is not None:
+			self.temp[member.name] = datetime.datetime.now()
+
+		self.data.to_csv(self.path)
+		a_file = open(self.temp_path, "wb")
+		pickle.dump(self.temp, a_file)
+		a_file.close()
+
+
+client = MyClient()
+client.run('ODExNTQ5MzYxOTA5MDA2MzY4.YCz0PQ.1VNSoiHbomNQMNwpqE5i-kwmRp8')
